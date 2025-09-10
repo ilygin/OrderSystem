637cfcb056f53ae99f660c9145ffee7f87daaa78
Igor Lygin <i.lygin@velvetech.dev>
2025-09-10 22:41:58 +0300

    Add OrderRepository class with method stubs
    Introduces the `OrderRepository` class implementing the `IOrderRepository` interface.
    Includes method stubs for creating, updating, deleting, and retrieving orders, all currently
    throwing `NotImplementedException`. Adds necessary using directives and organizes code
    under the `OrderSystem.Infrastructure.Repositories` namespace.

---
+++ OrderSystem/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using OrderSystem.Domain.Interfaces;
+using OrderSystem.Domain.Models;
+
+namespace OrderSystem.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public void CreateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+        public int UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+        public int DeleteOrder(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+        public Order? GetOrderById(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Order> GetAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
