27a470db24695f57752a087defa2f62f9483ec1f
Igor Lygin <lazycoast102@gmail.com>
2025-09-28 18:17:35 +0300

    Add IOrderService.cs (#60)
    * Add IOrderService.cs
    
    * Add fixes after code review

---
+++ OrderSystem/Domain/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using OrderSystem.Domain.DTO;
+using OrderSystem.Domain.Models;
+
+namespace OrderSystem.Domain.Interfaces
+{
+    public interface IOrderService
+    {
+        public Order GetOrder(Guid id);
+        public IEnumerable<Order> GetAllOrders();
+        public Order CreateOrder(OrderRequestDto data);
+        public Order? UpdateOrder(Guid id, OrderRequestDto data);
+        public bool DeleteOrder(OrderRequestDto data);
+    }
+}
